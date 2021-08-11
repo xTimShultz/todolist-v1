8511167131999d4baa0ecd9888d825d9fb116755
Tim Shultz <ttshultz@gmail.com>
2021-08-10 17:27:17 -0700

    Support addition of new items and display in an EJS scriptlet loop

--- app.js
+++ app.js
@@ -3,20 +3,32 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
+
     var today = new Date();
-    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var dayOfWeek1 = daysOfWeek[today.getDay()];
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     };
-    res.render("list", {kindOfDay: day, dayOfWeek: dayOfWeek1});
+    
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: day, newItems: items});
 });
 
+app.post("/", function(req, res) {
+    item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
 
 app.listen(3000, function() {
     console.log("Server listening on port 3000");
