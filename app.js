7f151e1bd57fac01ee398f502eb5a87b8550b2bc
Tim Shultz <ttshultz@gmail.com>
2021-08-10 20:29:23 -0700

    Add styling to todo list

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
@@ -21,13 +23,23 @@ app.get("/", function(req, res) {
     
     var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day, newItems: items});
+    res.render("list", {listTitle: day, newItems: items});
 });
 
 app.post("/", function(req, res) {
     item = req.body.newItem;
     items.push(item);
     res.redirect("/");
+});
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function (req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
 });
 
 app.listen(3000, function() {
